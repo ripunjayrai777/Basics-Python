d88b7777fb11e84d965a48be8f5344095c1b1da5
Ripunjay Rai <ripunjayrai777@gmail.com>
2026-01-21 10:39:08 +0530

    21-Jan

---
+++ day4a.py
@@ -0,0 +1,81 @@
+# example of while with continue--------------
+#34
+# count=0
+# while (count < 10) : 
+#         count = count + 1 
+#         if count == 4 :
+#             continue
+#         print ( count )
+
+
+
+
+#Examples of for loop:---------------
+
+#37
+# for i in (1,2,3,4,5) :      
+#     print ( 'The count is:', i )
+
+
+
+# for i in "NIELIT Gorakhpur":
+#     print(i)
+
+
+#38
+# count=0
+# for i in "Python Programming":
+#     if i in ('a','e', 'i', 'o', 'u') :
+#         count=count+1
+#         print(i)
+
+# print ('The count is:', count)
+
+
+
+
+#range(start, stop, steps)
+# count= 0
+# for i in range(1,11):
+#     if(i%2==0):
+#         count+=1
+#         print(i)
+# print(count)
+
+
+
+
+
+#40
+# count=0
+# for i in "Python Programming":
+#       if i in ('a', 'i', 'e', 'o', 'u') :
+#           count=count+1
+# else :
+#      print ('The count is:', count)
+
+
+
+
+
+
+
+
+#42
+# count=0
+# for i in [1 ,2 ,3, 4, 5,6]:
+#     count=count+1
+#     # if count==3 :
+#     #     break
+
+# print("Count : ", count)
+
+
+
+#45
+# count=0
+# for i in [1 ,2 ,3, 4, 5]:
+#     count=count+1
+#     # if count==3 :
+#     #     continue
+#     print("Count :",count)
