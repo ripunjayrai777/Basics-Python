5e79c3728b65beddfd9e3e81a54a2cf9dc8a115a
Ripunjay Rai <ripunjayrai777@gmail.com>
2026-01-22 13:02:17 +0530

    Day 5

--- day5.py
+++ day5.py
@@ -33,4 +33,39 @@
 
 
 
+#11
+#Empty Tuple
+# t1= ()
+# print(type(t1)) 
+
+
+# a tuple with one item
+# t2 = ("apple" , )
+# print(type(t2))
+
+
+#NOT a tuple
+# t3 = ("apple")
+# print(type(t3)) 
+
+
+#12 delete tuble
+# a = (1, 2, 3 )
+# del a 
+# print( a )                                   
+
+
+
+
+#15 set
+set1 = { 'physics', 'chemistry', 1997, 2000 }
+set2 = { 1, 2, 3, 4, 5, 6, 7  }
+print ("set1: ", set1)
+# print ("set1[1]: ", set1[1] )            # Index not allowed in Set
+# print ("tset2[3]: ", set2[-1] )          # Negative Index not allowed in set
+
+
+# set1[0] = 100 # not allowed
+# set3 = set1 + set2  # not allowed
+
 
