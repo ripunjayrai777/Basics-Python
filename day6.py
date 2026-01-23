682ce305149eaf936cb7f8a362366c907972f05a
Ripunjay Rai <ripunjayrai777@gmail.com>
2026-01-23 15:39:43 +0530

    day 6 partially

--- day6.py
+++ day6.py
@@ -1,1 +1,75 @@
-#
+#6 - eval() function evaluates the expression
+# x= 'print(55)'
+# eval(x)
+# x= eval( '10+5' )
+# print(x)	
+
+
+
+
+
+
+
+
+#7
+# x = format(0.5,'%')           	
+# y = format(255,'X')            
+# print(x)
+# print(y)
+
+
+
+# def add_num(a,b,) :
+   
+#      a+b
+
+# print(add_num(3,5))      
+
+# def AddOne(x,y) :
+#     print(x+y)
+
+
+# AddOne()
+
+
+# def my_function(x):
+   
+
+# my_function(3)
+# my_function(5)
+
+
+
+# def sum1(a , b):    
+#         r=a+b    
+#         print("sum =",r)
+
+
+# r=sum1(10,50)
+
+
+
+
+# def min(a,b) :
+#     if a<b :        
+#         r=a    
+#     else:       
+#         r=b    
+#     return(r)
+
+# print(min(8,5))
+# # max(9,3)
+
+f=120
+
+def  fact(n):   
+    f=1
+    i=1
+    while(i<=n):
+         f=f* i
+         i=i+1    
+    return(f)
+
+a=6
+
+print("fact=",fact(a))
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,4 @@
- 
+x = format(0.5,'%')           	
+# y = format(255.544545,'.2f')            
+# print(x)
+# print(y)
