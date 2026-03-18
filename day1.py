407e53836f7fc8039c089256837c762378381301
Ripunjay Rai <ripunjayrai777@gmail.com>
2026-03-18 09:10:32 +0530

    day ago

--- day1.py
+++ day1.py
@@ -1,2 +1,10 @@
-a=10
-print("ripu","kjdfkdj", "sdfgsdfg", a)
+x = [1, 2, 3]
+y = [1, 2, 3]
+print (x is y)
+
+t3 = ("apple")
+print(type(t3)) 
+A={1:100,2:200,3:300,6:400,5:500} 
+print (A.items( ) )
+print (A.keys( )) 
+print (A.values( ))
